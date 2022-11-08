0f21a344c22fb527bf501088d288a61daccbd850
Ben <ben@finch.org.uk>
2022-11-08 12:22:37 +0000

    commit coursework files

---
+++ coursework/Suko.pl
@@ -0,0 +1,22 @@
+%{-Begin Question 3.1-}
+%indices( IS, XS, ES )
+%  :-
+
+%{-End Question 3.1-}
+
+%{-Begin Question 3.2-}
+%possible( GRID )
+%  :-
+
+%{-End Question 3.2-}
+
+%{-Begin Question 3.3-}
+%acceptable( T0, T1, T2, T3, US, U, VS, V, WS, W, GRID)
+% :-
+
+%suko( T0, T1, T2, T3, US, U, VS, V, WS, W, GRID)
+%  :-
+
+%{-End Question 3.3-}
+
+%any main predicates for testing goes here
