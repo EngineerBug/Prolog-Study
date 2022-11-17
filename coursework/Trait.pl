9c85a67f6026394e1fe1ec8e14bb4df5d820629c
Ben <ben@finch.org.uk>
2022-11-17 16:33:58 +0000

    starting trait

--- coursework/Trait.pl
+++ coursework/Trait.pl
@@ -1,6 +1,18 @@
 %Begin Question 4.1
-%prime( N )
-%  :-
+%Fact: all numbers are divisible by 1.
+%Rule: for any numbers N and N-1:
+%		N is prime iff is is not divisible by all numbers below it, except 1.
+prime2( _, 1 ).
+prime2( N, D )
+    :- mod(N, D) =\= 0,
+    D1 is D - 1,
+    prime2(N, D1).
+
+%Rule: for any integer N:
+%   if the only factors of N are 1 and N, it is prime.
+prime( N )
+  :- N1 is N - 1,
+    prime2(N, N1).
 
 %End Question 4.1
 
@@ -21,22 +33,3 @@
 
 %any main predicates for testing goes here
 
-%Begin Question 4.1
-%Fact: all numbers are divisible by 1.
-%Rule: for any numbers N and N-1:
-%		N is prime iff is is not divisible by all numbers below it, except 1.
-prime2( _, 1 ).
-prime2( N, D )
-    :- 0 =\= mod(N, D),
-    D1 is D - 1,
-    prime2(N, D1).
-
-%Fact: 2 is the smallest prime number.
-prime( 2 ).
-prime( N )
-  :- N1 is N - 1,
-    0 =\= mod( N, N1 ),
-    prime( N1 ).
-
-main
-	:- prime2(10, 9).
