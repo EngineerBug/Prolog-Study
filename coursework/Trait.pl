71e2da393e58946b3b704f2fb3434eb000a10249
EngineerBug <ben@finch.org.uk>
2022-11-26 15:21:52 +0000

    corrected possibles

--- coursework/Trait.pl
+++ coursework/Trait.pl
@@ -16,41 +16,29 @@ prime( N )
 %End Question 4.1
 
 %Begin Question 4.2
+possiblePrint([], []).
+possiblePrint([X], [X]) :-!.
+possiblePrint(GRID, X) 
+    :- GRID = [H|T],
+    possiblePrint(T, T1),
+    append(L1, L2, T1), 
+    append(L1, [H], X1),
+    append(X1, L2, X).
 
-remover( _, [], []).
-remover( R, [R|T], T2) 
-  :- remover( R, T, T2).
-remover( R, [H|T], [H|T2]) 
-  :- H \= R, 
-  remover( R, T, T2).
+listToNum([E], E).
+listToNum([H|T], N)
+	:- length(T, L1),
+    listToNum(T, N1),
+    N is N1 + (H * (10 ^ L1)).
 
-%Fact: turning 0 into a list is [0]
-%Rule: for any number N,
-numToList( 0, [0] ).
-numToList( N, L )
-	:-N1 is N // 10, 
-  numToList(N1, L1), 
-  N2 is (N mod 10), 
-  append(L1, [N2] ,L).
-
-containOneToNine(_, _, _, []).
-containOneToNine([H1|T1], [H2|T2], [H3|T3], DIGITS)
-  :-remover(H1, DIGITS, D1),
-  remover(H2, D1, D2),
-  remover(H3, D2, D3),
-  containOneToNine(T1, T2, T3, D3).
-
-%generate all triples that are valid bearings
-possible( A, B, C )
-	:- A < 360,
-	B < 360,
-  C < 360,
-  %the following five lines turn X, Y and Z into a list of digits
-  numToList(A, AS),
-  numToList(B, BS),
-  numToList(C, CS),
-  %check that the bearings contain 1..9
-  containOneToNine(AS, BS, CS, [1,2,3,4,5,6,7,8,9]).
+possible(X,Y,Z)
+	:-possiblePrint([1,6,8,2,7,9,3,4,5],[A,B,C,D,E,F,G,H,I]),
+    listToNum([A,B,C], X),
+    X < 360,
+    listToNum([D,E,F], Y),
+    Y < 360,
+    listToNum([G,H,I], Z),
+    Z <360.
 
 %End Question 4.2
 
@@ -65,13 +53,16 @@ isSet([H|T])
 %integer divide (//) all bearings by 90 and check they are all different
 %check if ONE set of bearings is acceptable
 acceptable( A, B, C ) 
-	:- \+ prime(A),
+	%check none of the bearings are prime
+	:- 
+    \+ prime(A),
 	\+ prime(B),
 	\+ prime(C),
-  A1 is A // 90,
-  B1 is B // 90,
-  C1 is C // 90,
-  isSet([A1, B1, C1]).
+    %check all the bearings are in different quadrents
+    A1 is A // 90,
+    B1 is B // 90,
+    C1 is C // 90,
+    isSet([A1, B1, C1]).
 
 %given the question, I should be able 
 trait( A, B, C )
@@ -82,13 +73,9 @@ trait( A, B, C )
 
 %any main predicates for testing goes here
 main
-	%:- prime(19). %true
+	:- prime(19). %true
 	%:- prime(4). %false
-	%:- prime(11110). % false
-	%:- numToList(1234, X), write(X). % [1,2,3,4] true
-	:- containOneToNine([1,2], [3], [4], [6,5,4,3,2,1]). %true
-	%:- containOneToNine([1,2,4], [], [], [4,3,2,1]). %false
-	%:- containOneToNine([1,2,3],[4,5,6], [7,8,9] [1,2,3,4,5,6,7,8,9]). %true
+	%:- prime(11110). % false.
 	%:- possible(123,456,789). %true x1000
 	%:- isSet([1,2,3,4]). %true
 	%:- isSet([1,1,1,2]). %false
